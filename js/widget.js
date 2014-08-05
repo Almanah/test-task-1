fc3fcf57590176f251e38ce176e810cda4a998a6
unknown <Denis_Savchyshkin@EPBYMINW4421.minsk.epam.com>
2014-08-05 15:10:13 +0300

    Initial commit

---
+++ js/widget.js
@@ -0,0 +1,27 @@
+var Widget = Backbone.model.extend({
+
+});
+
+var TwitterWidget = Widget.extend({
+
+});
+
+var VineWidget = TwitterWidget.extend({
+
+});
+
+var InstagramWidget = Widget.extend({
+
+});
+
+var InstagramFotoWidget = InstagramWidget.extend({
+
+});
+
+var InstagramVideoWidget = InstagramWidget.extend({
+
+});
+
+var AppModel = Backbone.model.extend({
+
+});
