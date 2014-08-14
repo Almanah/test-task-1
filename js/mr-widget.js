02a789eac038f62cb38f1dcd521108b3bfb98f35
unknown <Denis_Savchyshkin@epam.com>
2014-08-14 17:57:00 +0300

    Added date logic and some styles

--- js/mr-widget.js
+++ js/mr-widget.js
@@ -1,14 +1,16 @@
 (function() {
 
 	//	Set your parametres here
-	var URL = 'http://api.massrelevance.com/jmskey/vine-test.json',	// Url of your stream
+	var URL = 'http://api.massrelevance.com/jmskey/vmn.json',	// Url of your stream
 		REFRESH_INTERVAL = 5000;	// Refresh time in milliseconds
 
 	//Backbone logic
 	var Post = Backbone.Model.extend({
 		initialize: function() {
-			this.set('content', '');
-			this.set('counts', {});
+			this.set({
+				content: '',
+				counts: {}
+			});
 		}
 	});
 
@@ -41,6 +43,9 @@
 		},
 
 		render: function(postID, isAnimated) {
+			//First set fresh time
+			var model = this.collection.get('c' + postID);
+			model.set('time', parseDate(model.get('rawTime')));
 			//	Set animation for all posts except first or in case of updated content
 			if(postID !== 1 && isAnimated) {
 				this.$el.fadeOut('400', (function(view) {
@@ -50,7 +55,7 @@
 					}
 				})(this));
 			} else {
-				this.$el.html(this.template(this.collection.get('c' + postID).toJSON()));
+				this.$el.html(this.template(model.toJSON()));
 			}
 			return this;
 		}
@@ -83,28 +88,28 @@
 			userImage: element.user.profile_image_url,
 			userUrl: 'http://twitter.com/' + newPost.get('userName'),
 			postUrl: 'http://twitter.com/' + newPost.get('userName') + '/status/' + element.id_str,
-			time: element.created_at,
+			rawTime: element.created_at,
 			counts: {
 				favorites: {
 					value: element.favorite_count,
-					imageSrc: ''
+					imageSrc: 'https://si0.twimg.com/images/dev/cms/intents/icons/favorite_hover.png'
 				},
 				retweets: {
 					value: element.retweet_count,
-					imageSrc: ''
+					imageSrc: 'https://si0.twimg.com/images/dev/cms/intents/icons/retweet_hover.png'
 				}
 			}
 		});
 
 		//Check vine or not
 		if(expandedURL.indexOf('vine.co/v/') !== -1) {				
-			var url = 'http://api.embed.ly/1/oembed?url=' + expandedURL + '&height=220';
+			var url = 'http://api.embed.ly/1/oembed?url=' + expandedURL + '&height=230';
 			getEmbedVideo(newPost, url);
 			newPost.set('type', 'vine');
 		} else {
 			newPost.set({
 				type: 'tweet',
-				content: element.text
+				content: '<div class = "tweet">' + element.text + '</div>'
 			});				
 		}
 		return newPost;
@@ -115,7 +120,7 @@
 
 		newPost.set({
 			type: element.type,
-			time: element.created_time * 1000,
+			rawTime: element.created_time * 1000,
 			userName: element.user.username,
 			userImage: element.user.profile_picture,
 			userUrl: 'http://instagram.com/' + newPost.get('userName'),
@@ -136,7 +141,7 @@
 			newPost.set('content', '<a href = "' + newPost.get('postUrl') + '" target = "_blank"><img src = "' + element.images.standard_resolution.url + '"></a>');
 		} else {
 			var videoUrl = element.link,
-				url = 'http://api.embed.ly/1/oembed?url=' + videoUrl + '&height=220';
+				url = 'http://api.embed.ly/1/oembed?url=' + videoUrl + '&height=230';
 			getEmbedVideo(newPost, url);
 		}
 		return newPost;
@@ -158,12 +163,40 @@
 		request.send();
 	};
 
+	var dateConstants = {
+		msInDay: 86400000,
+		msInHour: 3600000,
+		msInMinute: 60000,
+		monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec']
+	};
+
+	var parseDate = function(rawDate) {
+		var postTime = (new Date(rawDate)).getTime(),
+			currentTime = (new Date).getTime(),			
+			timestamp = null;
+
+		if((currentTime - postTime) <= dateConstants.msInDay) {
+			var msAgo = currentTime - postTime,
+				hoursAgo = Math.floor(msAgo / dateConstants.msInHour);
+			timestamp = hoursAgo > 0 ? (hoursAgo + (hoursAgo > 1 ? ' hours' : ' hour') + ' ago') : (Math.ceil(msAgo / dateConstants.msInMinute) + ' min ago');
+		} else {
+			var postDate = new Date(postTime);
+			timestamp = dateConstants.monthNames[postDate.getMonth()] + ' ' + postDate.getDate() + ', ' + postDate.getFullYear();
+		}
+		console.log(postTime, currentTime, timestamp);
+		return timestamp;		
+	};
+
 	//	Widget start point 
 	var createQueue = (function () {
 		var xhr = new XMLHttpRequest();
 			xhr.onload = function() {
-				var	postsQueue = new PostsQueue(parseResponse(this.responseText)),
-					appView = new WidgetView({collection: postsQueue});
+				if(this.status === 200 && this.responseText.length !== 0) {
+					var	postsQueue = new PostsQueue(parseResponse(this.responseText)),
+					appView = new WidgetView({collection: postsQueue});					
+				} else {
+					$('#MRWidget').html('Sorry, some kind of error has occurred<br>' + this.status + ' ' + this.statusText);
+				}
 			};
 		xhr.open('GET', URL, true);
 		xhr.send();
