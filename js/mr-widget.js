ee91592033273e06d449d13fad043201b38490f8
Almanah <Denis_Savchyshkin@epam.com>
2014-08-19 18:42:06 +0300

    Remove storage, new date logic, start changing arcitecture

--- js/mr-widget.js
+++ js/mr-widget.js
@@ -1,7 +1,7 @@
 (function() {
 
 	//	Set your parametres here
-	var URL = 'https://api.massrelevance.com/jmskey/ema-instagram-video.json',	// Url of your stream
+	var URL = 'https://api.massrelevance.com/jmskey/vmn.json',	// Url of your stream
 		REFRESH_INTERVAL = 5000;	// Refresh time in milliseconds
 
 	//Backbone logic
@@ -14,15 +14,59 @@
 		}
 	});
 
-	var PostsQueue = Backbone.Collection.extend({});
+	var TwitterPost = Backbone.Model.extend({
+		parse: function(data, options) {
+			var	numOfUrls = data.entities.urls.length,
+				expandedURL = numOfUrls > 0 ? data.entities.urls[numOfUrls - 1].expanded_url : '';
+
+			this.userName = data.user.screen_name;
+			this.userImage = data.user.profile_image_url;
+			this.userUrl = 'http://twitter.com/' + data.user.screen_name;
+			this.postUrl = 'http://twitter.com/' + data.user.screen_name + '/status/' + data.id_str;
+			this.rawTime = data.created_at;
+			this.counts = {
+					favorites: data.favorite_count,
+					retweets: data.retweet_count
+			};
+			//Check vine or not
+			if(expandedURL.indexOf('vine.co/v/') !== -1) {
+				var url = prefix + '://api.embed.ly/1/oembed?url=' + expandedURL + '&height=230';
+				getEmbedVideo(this, url);
+				this.type = 'vine';
+			} else {
+				this.type = 'tweet';
+				this.content = '<div class = "tweet">' + parseTweet(data.text) + '</div>'				
+			}
+		}
+	});
+
+	var InstagramPost = Post.extend({
+
+	});
+
+	var PostsQueue = Backbone.Collection.extend({
+		initialize: function() {
+
+		}
+	});
 
 	var WidgetView = Backbone.View.extend({
 		el: '#MRWidget',
 		template: _.template($('#MRWidgetTemplate').html()),
+		dateConstants: {
+			msInYear: 31536000000,	// year is 365 days
+			msInMonth: 2628000000,	// month is 1/12 of year
+			msInWeek: 604800000,
+			msInDay: 86400000,
+			msInHour: 3600000,
+			msInMinute: 60000,
+			monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec']
+		},
 
 		initialize: function() {		
-			this.currentPost = window.sessionStorage.mrWingetCurrPost || 0;
+			this.currentPost = 0;
 			this.postsCount = this.collection.length;
+			//this.postsCount = 1;
 			//	Need to rerender view if content of current post updates
 			this.collection.on('change:content', (function(view) {
 				return function(e) {
@@ -35,8 +79,7 @@
 		},
 
 		startRefreshing: function() {
-			//this.currentPost++;
-			window.sessionStorage.mrWingetCurrPost = this.currentPost++;
+			this.currentPost++;
 			this.render(this.currentPost, true);			
 			if(this.currentPost === this.postsCount)
 				this.currentPost = 0;
@@ -47,7 +90,9 @@
 		render: function(postID, isAnimated) {
 			//First set fresh time
 			var model = this.collection.get('c' + postID);
-			model.set('time', parseDate(model.get('rawTime')));
+			console.log(model);
+			model.set('time', this.parseDate(model.get('rawTime')));
+			console.log(model);
 			//	Set animation for all posts except first or in case of updated content
 			if(postID !== 1 && isAnimated) {
 				this.$el.fadeOut('400', (function(view) {
@@ -60,6 +105,46 @@
 				this.$el.html(this.template(model.toJSON()));
 			}
 			return this;
+		},
+
+		parseDate: function(rawDate) {
+			var postTime = (new Date(rawDate)).getTime(),
+				currentTime = (new Date).getTime(),
+				msAgo = currentTime - postTime,
+				timestamp = '';
+			if((msAgo / this.dateConstants.msInYear) >= 1) {
+				var years = Math.floor(msAgo / this.dateConstants.msInYear);
+				timestamp = years + (years > 1 ? ' years' : ' year');
+			} else if((msAgo / this.dateConstants.msInMonth) >= 1) {
+				var months = Math.floor(msAgo / this.dateConstants.msInMonth);
+				timestamp = months + (months > 1 ? ' months' : ' month');
+			} else if((msAgo / this.dateConstants.msInWeek) >= 1) {
+				var weeks = Math.floor(msAgo / this.dateConstants.msInWeek);
+				timestamp = weeks + (weeks > 1 ? ' weeks' : ' week');
+			} else if((msAgo / this.dateConstants.msInDay) >= 1) {
+				var days = Math.floor(msAgo / this.dateConstants.msInDay);
+				timestamp = days + (days > 1 ? ' days' : ' day');
+			} else if((msAgo / this.dateConstants.msInHour) >= 1) {
+				var hours = Math.floor(msAgo / this.dateConstants.msInHour);
+				timestamp = hours + (hours > 1 ? ' hours' : ' hour');
+			} else if((msAgo / this.dateConstants.msInMinute) > 0) {
+				var min = Math.ceil(msAgo / this.dateConstants.msInMinute);
+				timestamp = min + ' min';
+			} else {
+				return 'Post link';	// save user's opportunity to link post if time is incorrect
+			}
+			return timestamp + ' ago';
+		},
+
+		embedContent: function(model) {
+			var type = model.get('type');
+			if(type === 'photo') {
+
+			} else if(type === 'video') {
+
+			} else if(type === 'tweet') {
+
+			}
 		}
 	});
 
@@ -70,7 +155,7 @@
 
 		rawPosts.forEach(function(element) {
 			if(element.network === 'twitter') {
-				posts.push(createTwitterModel(element));
+				posts.push(new TwitterPost(element));
 			} else if(element.network === 'instagram') {
 				posts.push(createInstagramModel(element));
 			} else {
@@ -166,49 +251,19 @@
 		request.send();
 	};
 
-	var dateConstants = {
-		msInDay: 86400000,
-		msInHour: 3600000,
-		msInMinute: 60000,
-		monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec']
-	};
-
-	var parseDate = function(rawDate) {
-		var postTime = (new Date(rawDate)).getTime(),
-			currentTime = (new Date).getTime(),			
-			timestamp = null;
-
-		if((currentTime - postTime) <= dateConstants.msInDay) {
-			var msAgo = currentTime - postTime,
-				hoursAgo = Math.floor(msAgo / dateConstants.msInHour);
-			timestamp = hoursAgo > 0 ? (hoursAgo + (hoursAgo > 1 ? ' hours' : ' hour') + ' ago') : (Math.ceil(msAgo / dateConstants.msInMinute) + ' min ago');
-		} else {
-			var postDate = new Date(postTime);
-			timestamp = dateConstants.monthNames[postDate.getMonth()] + ' ' + postDate.getDate() + ', ' + postDate.getFullYear();
-		}
-		return timestamp;		
-	};
-
 	var prefix = URL.indexOf('https') === 0 ? 'https' : 'http';
 	//	Widget start point 
 	var createQueue = (function () {
-		if(window.sessionStorage.mrWidgetStream === undefined) {
-			var xhr = new XMLHttpRequest();
-				xhr.onload = function() {
-					if(this.status === 200 && this.responseText.length !== 0) {
-						var	postsQueue = new PostsQueue(parseResponse(this.responseText)),
-						appView = new WidgetView({collection: postsQueue});
-
-						window.sessionStorage.mrWidgetStream = this.responseText;
-					} else {
-						$('#MRWidget').html('Sorry, some kind of error has occurred<br>' + this.status + ' ' + this.statusText);
-					}
-				};
-			xhr.open('GET', URL, true);
-			xhr.send();
-		} else {
-			var	postsQueue = new PostsQueue(parseResponse(window.sessionStorage.mrWidgetStream)),
-			appView = new WidgetView({collection: postsQueue});
-		}
+		var xhr = new XMLHttpRequest();
+			xhr.onload = function() {
+				if(this.status === 200 && this.responseText.length !== 0) {
+					var	postsQueue = new PostsQueue(parseResponse(this.responseText)),
+					appView = new WidgetView({collection: postsQueue});
+				} else {
+					$('#MRWidget').html('Sorry, some kind of error has occurred<br>' + this.status + ' ' + this.statusText);
+				}
+			};
+		xhr.open('GET', URL, true);
+		xhr.send();
 	})();
 })();
